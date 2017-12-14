20e5b32306d88128f4a82b9dfe358b64a3041feb
Suratshalaka <suratshalakakhatkale@gmail.com>
2017-12-14 15:04:00 +0530

    hi

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,24 @@
+class AnswersController < ApplicationController
+
+		
+	def create
+		@question = Question.find(params[:question_id])
+		@answer = @question.answers.build(answers_params)
+		@answer.user = current_user
+		@answer.save
+
+		redirect_to questions_path
+	end
+	
+    def destroy
+	     @question = Question.find(params[:question_id])
+	     @answer = @question.answers.find(params[:id]).destroy	
+	     redirect_to questions_path
+   end
+	
+	private
+
+	def answers_params
+	  params.require(:answer).permit(:content)
+	end       
+end
